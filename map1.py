5cbefc00b79df9de443d03cc6645b483c9d665bb
mawais54013 <mawais54013@gmail.com>
2022-06-16 11:34:06 -0700

    Add Lat and Long

--- map1.py
+++ map1.py
@@ -1,8 +1,17 @@
 import folium 
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location = [38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color="green")))
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location = [lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color="green")))
+
 map.add_child(fg)
 
 map.save("Map1.html")
